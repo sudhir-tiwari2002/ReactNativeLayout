bf8d61bc0ac50202aa7a0fe46a432cb7f1b67233
sudhir.pw <sudhir.tiwari@pw.live>
2023-11-28 16:44:25 +0530

    box modal in layout

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: "lightblue" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "lightgreen" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "lightpink" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "hotpink" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "gray" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "skyblue" }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 60,
+    borderWidth: 6,
+    borderColor: "red",
   },
 });

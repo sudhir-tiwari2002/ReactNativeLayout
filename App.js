525a0bce7ce462d5b9d82ff5032129f367dd5698
sudhir.pw <sudhir.tiwari@pw.live>
2023-11-28 17:00:47 +0530

    flex layout

--- App.js
+++ App.js
@@ -3,20 +3,24 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
+    // <View style={{flex:1, backgroundColor:"plum"}}> 
+
+    // </View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "lightblue" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "lightgreen" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "lightpink" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "hotpink" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "gray" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "purple" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "skyblue" }}>Box 7</Box>
+      <Box style={{ backgroundColor: "lightblue" ,flex:1}}>Box 1</Box>
+      <Box style={{ backgroundColor: "lightgreen" ,flex:2}}>Box 2</Box>
+      <Box style={{ backgroundColor: "lightpink" ,flex:3}}>Box 3</Box>
+      <Box style={{ backgroundColor: "hotpink" , flex:4}}>Box 4</Box>
+      <Box style={{ backgroundColor: "gray" ,flex:5}}>Box 5</Box>
+      <Box style={{ backgroundColor: "purple" ,flex:6}}>Box 6</Box>
+      <Box style={{ backgroundColor: "skyblue" ,flex:7}}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
     marginTop: 60,
     borderWidth: 6,
     borderColor: "red",

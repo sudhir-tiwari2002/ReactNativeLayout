70ee738178d2f7709018a7a294c762fb9877e251
sudhir.pw <sudhir.tiwari@pw.live>
2023-12-06 11:21:31 +0530

    align self in flex box layout

--- App.js
+++ App.js
@@ -7,13 +7,13 @@ export default function App() {
 
     // </View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "lightblue" , }}>Box 1</Box>
-      <Box style={{ backgroundColor: "lightgreen" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "lightpink" }}>Box 3</Box>
-      {/* <Box style={{ backgroundColor: "hotpink" , flex:4}}>Box 4</Box>
-      <Box style={{ backgroundColor: "gray" ,flex:3}}>Box 5</Box>
-      <Box style={{ backgroundColor: "purple" ,flex:2}}>Box 6</Box>
-      <Box style={{ backgroundColor: "skyblue" ,flex:1}}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "lightblue" , alignSelf: "flex-start" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "lightgreen" ,alignSelf:"flex-end"  }}>Box 2</Box>
+      <Box style={{ backgroundColor: "lightpink" , alignSelf:"center"}}>Box 3</Box>
+      <Box style={{ backgroundColor: "hotpink" , alignSelf:"strech"}}>Box 4</Box>
+      <Box style={{ backgroundColor: "gray" , alignSelf:"auto"}}>Box 5</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "skyblue",alignSelf:"baseline" }}>Box 7</Box>
     </View>
   );
 }
@@ -21,13 +21,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    flexDirection:"row",
+    
+    // flexDirection:"row",
     // justifyContent:"space-between",
     // alignItems:"baseline",
     // alignItems:"center",
     // alignItems:"flex-end",
     // alignItems:"flex-start",
-    alignItems:"stretch",
+    alignItems:"flex-end", // align self is depend on the align item position 
     marginTop: 60,
     borderWidth: 6,
     borderColor: "red",
